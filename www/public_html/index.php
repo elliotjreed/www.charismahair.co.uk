0683df88072118fc74379aea919d4a12a27364b2
Elliot J. Reed <git-contact@elliotjreed.com>
2017-01-18 16:32:30 +0000

    map edit

--- www/public_html/index.php
+++ www/public_html/index.php
@@ -320,7 +320,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="map">
-                        <iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;sll=52.1980745,0.9562433&amp;ie=UTF8&amp;hq=Charisma+Hair&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
+                        <iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;aq=0&amp;oq=twitter&amp;sll=52.1980745,0.9562433&amp;ie=UTF8&amp;hq=Charisma+Hair&amp;t=m&amp;z=15&amp;iwloc=A&amp;output=embed"></iframe>
                     </div>
                 </div>
             </div>
