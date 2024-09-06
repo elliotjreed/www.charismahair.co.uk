87ea3d728af25f766e38330fa81640e416dd93bf
Elliot J. Reed <git@elliotjreed.com>
2024-09-06 11:21:00 +0100

    Adds backup phone number, removes COVID guidelines, and updates dependencies. Prepares for use with Cloudflare pages.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -13,7 +13,7 @@ module.exports = [
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "[name].[chunkhash].js",
-      publicPath: "/",
+      publicPath: "./",
     },
     optimization: {
       minimize: true,
